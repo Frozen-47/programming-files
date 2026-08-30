9c9233d4220bb0fce81fdb67ed0bb5fe744c3583
Sabareesh G <sabareeshgm47@gmail.com>
2026-08-30 15:20:00 +0530

    feat(cpp): implement linked list stack with dynamic memory management

---
+++ Sabareesh_Codes/C++ Programs/Programs & Examples/stack_using_linked_list.cpp
@@ -0,0 +1,51 @@
+﻿// C++ Program: Stack using Linked List
+// Date: 2026-08-30
+
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+class LinkedListStack {
+private:
+    Node* top;
+public:
+    LinkedListStack() : top(nullptr) {}
+
+    void push(int val) {
+        Node* newNode = new Node(val);
+        newNode->next = top;
+        top = newNode;
+    }
+
+    int pop() {
+        if (!top) {
+            cout << "Stack Underflow\n";
+            return -1;
+        }
+        int val = top->data;
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+        return val;
+    }
+
+    bool isEmpty() const {
+        return top == nullptr;
+    }
+};
+
+int main() {
+    LinkedListStack s;
+    s.push(100);
+    s.push(200);
+    s.push(300);
+    cout << "Popped: " << s.pop() << endl;
+    cout << "Popped: " << s.pop() << endl;
+    return 0;
+}
